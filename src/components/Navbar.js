9207923bf76d21bc9bb01fa58c44451eae314648
VelOuria <sofleos@gmail.com>
2021-11-26 23:25:27 -0600

    added navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
 import React from "react"
+import AppBar from "@mui/material/AppBar"
+import { makeStyles } from "@mui/styles"
+import { fontWeight, padding } from "@mui/system"
+
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: 4
+  }
+}))
 
 function NavBar() {
-  return <h2>Sofía De la Cueva</h2>
+  const classes = useStyles()
+  return (
+    <AppBar
+      sx={{
+        padding: 3,
+        backgroundColor: "Gainsboro",
+        color: "Black",
+        fontSize: "1.7rem",
+        fontWeight: "bold"
+      }}
+    >
+      Sofía De la Cueva
+    </AppBar>
+  )
 }
 
 export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
@@ -6,26 +5,8 @@ import * as serviceWorker from "./serviceWorker"
 import Router from "./Router"
 
 ReactDOM.render(<Router />, document.getElementById("root"))
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
->>>>>>> 52eeaa0560387a009edfd75dd2777ba81afc62d7
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-<<<<<<< HEAD
 serviceWorker.unregister()
-=======
-serviceWorker.unregister();
->>>>>>> 52eeaa0560387a009edfd75dd2777ba81afc62d7
